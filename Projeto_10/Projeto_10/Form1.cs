eb906b4a5e7f48f803d7266d9e8bb290518e28cc
GermanoC <germanosantos.cesar@gmail.com>
2023-03-14 20:20:23 -0300

    14/03/2023
    Atividade borboleta

---
+++ Projeto_10/Projeto_10/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projeto_10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+
+            if(e.KeyCode== Keys.Up)
+            {
+                label1.Location= new Point(label1.Location.X, label1.Location.Y - 10);
+            }
+            if(e.KeyCode == Keys.Down)
+            {
+                label1.Location = new Point(label1.Location.X, label1.Location.Y + 10);
+            }
+            if(e.KeyCode == Keys.Right)
+            {
+                label1.Location = new Point(label1.Location.X + 10, label1.Location.Y);
+            }
+            if(e.KeyCode == Keys.Left)
+            {
+                label1.Location = new Point(label1.Location.X - 10, label1.Location.Y);
+            }
+            
+
+        }
+    }
+}
